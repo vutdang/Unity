e59a184b899ac6b0c2a4b4003cadb1b20c1e4bce
Vu Dang <vu@collamart.com>
2016-05-04 00:43:10 -0700

    Recipe: Selecting buttons through the keyboard
    Review page 20-23, Unity UI Cookbook book

---
+++ Assets/ButtonThroughKeySelection.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using System.Collections;
+
+public class ButtonThroughKeySelection : MonoBehaviour {
+
+    public string key;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(key))
+        {
+            EventSystem.current.SetSelectedGameObject(this.gameObject);
+        }
+	
+	}
+}
